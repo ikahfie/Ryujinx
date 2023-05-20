4f2e6dabaa94a0d0970f55ae1afdf490c367c48d
Dan <dan@wadoku.de>
2023-05-20 22:48:55 +0200

    address feedback

--- src/Ryujinx.Graphics.Shader/Instructions/InstEmitPredicate.cs
+++ src/Ryujinx.Graphics.Shader/Instructions/InstEmitPredicate.cs
@@ -50,7 +50,7 @@ namespace Ryujinx.Graphics.Shader.Instructions
             context.Copy(Register(op.DestPred, RegisterType.Predicate), p0Res);
             context.Copy(Register(op.DestPredInv, RegisterType.Predicate), p1Res);
         }
-        
+
         public static void P2rC(EmitterContext context)
         {
             InstP2rC op = context.GetOp<InstP2rC>();
@@ -58,18 +58,18 @@ namespace Ryujinx.Graphics.Shader.Instructions
             Operand srcA = GetSrcReg(context, op.SrcA);
             Operand dest = GetSrcReg(context, op.Dest);
             Operand mask = GetSrcCbuf(context, op.CbufSlot, op.CbufOffset);
-            
+
             EmitP2r(context, srcA, dest, mask, op.ByteSel, op.Ccpr);
         }
-        
+
         public static void P2rI(EmitterContext context)
         {
             InstP2rI op = context.GetOp<InstP2rI>();
 
             Operand srcA = GetSrcReg(context, op.SrcA);
             Operand dest = GetSrcReg(context, op.Dest);
-            Operand mask = Const(op.Imm20 & 0xff);
-            
+            Operand mask = GetSrcImm(context, op.Imm20);
+
             EmitP2r(context, srcA, dest, mask, op.ByteSel, op.Ccpr);
         }
 
@@ -80,10 +80,10 @@ namespace Ryujinx.Graphics.Shader.Instructions
             Operand srcA = GetSrcReg(context, op.SrcA);
             Operand dest = GetSrcReg(context, op.Dest);
             Operand mask = GetSrcReg(context, op.SrcB);
-            
+
             EmitP2r(context, srcA, dest, mask, op.ByteSel, op.Ccpr);
         }
-        
+
         private static void EmitP2r(
             EmitterContext context,
             Operand srcA,
@@ -97,10 +97,11 @@ namespace Ryujinx.Graphics.Shader.Instructions
             {
                 return context.ICompareNotEqual(context.BitwiseAnd(value, Const(1 << bit)), Const(0));
             }
-            
+
             int count = ccpr ? RegisterConsts.FlagsCount : RegisterConsts.PredsCount;
             int shift = (int)byteSel * 8;
-
+            mask = context.BitwiseAnd(mask, Const(0xff));
+            
             Operand insert = Const(0);
             for (int i = 0; i < count; i++)
             {
@@ -112,7 +113,7 @@ namespace Ryujinx.Graphics.Shader.Instructions
                 insert = context.ConditionalSelect(Test(context.ShiftRightU32(mask, Const(1)), i), context.BitwiseOr(insert, bit), insert);
             }
 
-            Operand masked = context.BitwiseAnd(srcA, context.INegate(context.ShiftLeft(mask,Const( shift))));
+            Operand masked = context.BitwiseAnd(srcA, context.INegate(context.ShiftLeft(mask, Const(shift))));
 
             Operand res = context.BitwiseOr(masked, insert);
 
